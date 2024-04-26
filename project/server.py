cc0d9386c72056731f991fff90f81c1ca2b64e82
mac <>
2024-04-26 15:59:21 +0800

    init websocket

---
+++ project/server.py
@@ -0,0 +1,26 @@
+import asyncio
+import websockets
+import websockets.exceptions
+
+# lsof -i :8080
+"""
+kill -9 2543
+"""
+
+'''
+python 代码注释
+'''
+
+async def echo(websocket, path):
+    try:
+        async for message in websocket:
+            print(f"Received message from client: {message}")
+            await websocket.send(message)
+    except websockets.exceptions.ConnectionClosedError as e:
+        print(f"Connection closed unexpectedly: {e}")
+
+async def start_server():
+    server = await websockets.serve(echo, "localhost", 8765)
+    await server.wait_closed()
+
+asyncio.run(start_server())
